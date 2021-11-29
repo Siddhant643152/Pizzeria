93300ce5856693b9a6fa15cf5e95be1cc3af3d5f
Siddhant643152 <81233517+Siddhant643152@users.noreply.github.com>
2021-11-29 16:54:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+menu_list_array = ["Peppy paner","Mexican Green Wave","Delgue Veggie","Non Veg Supreme","Chiken Dominos Pizza"]
+function getmenu(){
+    var htmldata = "";
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata + menu_list_array[i] + '<br>';
+    }
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+function add_item(){
+    var htmldata;
+    var imgtags='<img id="im1" src="images/pizzaImg.png"/>'
+    var item=document.getElementById("add_item").value;
+    menu_list_array.sort();
+    htmldata="";
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata + imgtags + menu_list_array[i]+'<br>'; 
+    }
+    document.getElementById("display_addmenu").innerHTML = htmldata;
+}
+function add_top(){
+    var type=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    add_item();
+}
